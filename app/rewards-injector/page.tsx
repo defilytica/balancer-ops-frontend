28e76706454668989bd9cebe2c2b4fa54572f3be
Silas Stulz <silas.stulz@gmail.com>
2024-07-11 11:11:25 +0200

    setting up foundation

---
+++ app/rewards-injector/page.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+export default function PayloadBuilder() {
+    return (
+        <Box>
+            <Box p={4}>
+                <Heading>Rewards Injector</Heading>
+                <Text mt={4}>
+                    This is the Reward Injector page.
+                </Text>
+            </Box>
+        </Box>
+    );
+};
