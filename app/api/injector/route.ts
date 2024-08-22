38fb4b3d2324be9fe30d221210a5eecdc4b2d0ec
Silas Stulz <silas.stulz@gmail.com>
2024-08-22 10:48:13 +0200

    hidden api key in backend

---
+++ app/api/injector/route.ts
@@ -0,0 +1,127 @@
+// pages/api/injector-data.js
+
+import {ethers} from "ethers";
+import {InjectorABIV1} from "@/abi/InjectorV1";
+import {ERC20} from "@/abi/erc20";
+import {poolsABI} from "@/abi/pool";
+import {gaugeABI} from "@/abi/gauge";
+import MainnetLogo from "@/lib/shared/imgs/mainnet.svg";
+import PolygonLogo from "@/lib/shared/imgs/polygon.svg";
+import OptimismLogo from "@/lib/shared/imgs/optimism.svg";
+import AvalancheLogo from "@/lib/shared/imgs/avalancheLogo.svg";
+import ArbitrumLogo from "@/lib/shared/imgs/arbitrum.svg";
+import GnosisLogo from "@/lib/shared/imgs/gnosis.svg";
+import BaseLogo from "@/lib/shared/imgs/base.svg";
+import zkevmLogo from "@/lib/shared/imgs/Polygon-zkEVM.png";
+import {NextRequest, NextResponse} from "next/server";
+
+const networks: Record<string, NetworkInfo> = {
+    mainnet: {logo: MainnetLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=ethereum&dkey="},
+    polygon: {logo: PolygonLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=polygon&dkey="},
+    optimism: {logo: OptimismLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=optimism&dkey="},
+    avalanche: {logo: AvalancheLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=avalanche&dkey="},
+    arbitrum: {logo: ArbitrumLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=arbitrum&dkey="},
+    gnosis: {logo: GnosisLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=gnosis&dkey="},
+    base: {logo: BaseLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=base&dkey="},
+    zkevm: {logo: zkevmLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=polygon-zkevm&dkey="}
+};
+
+const tokenDecimals: Record<string, number> = {
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": 6, // mainnet
+    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": 6, // polygon
+    "0xaf88d065e77c8cc2239327c5edb3a432268e5831": 6, // arbitrum
+    "0xddafbb505ad214d7b80b1f830fccc89b60fb7a83": 6, // gnosis
+    "0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035": 6, // zkevm
+    "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e": 6, // avalanche
+    "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913": 6, // base
+    "0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca": 6, // base USDbC
+    "0x0b2c639c533813f4aa9d7837caf62653d097ff85": 6, // OP USDC
+};
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const address = searchParams.get('address');
+    const network = searchParams.get('network');
+    const token = searchParams.get('token');
+
+    if (!address || !network || !token) {
+        return NextResponse.json({ error: 'Missing required parameters' }, { status: 400 });
+    }
+
+    const rpcUrl = `${networks[network].rpc}${process.env.DRPC_API_KEY}`;
+    const provider = new ethers.JsonRpcProvider(rpcUrl);
+    const contract = new ethers.Contract(address, InjectorABIV1, provider);
+
+    try {
+        const [watchList, injectorTokenAddress] = await Promise.all([
+            contract.getWatchList(),
+            contract.getInjectTokenAddress()
+        ]);
+
+        const tokenInfo = await fetchTokenInfo(injectorTokenAddress, provider);
+        const gauges = await fetchGaugeInfo(watchList, contract, provider, address);
+
+        return NextResponse.json({ tokenInfo, gauges });
+    } catch (error) {
+        console.error("Error:", error);
+        return NextResponse.json({ error: 'An error occurred while fetching data' }, { status: 500 });
+    }
+}
+
+async function fetchTokenInfo(tokenAddress, provider) {
+    const tokenContract = new ethers.Contract(tokenAddress, ERC20, provider);
+    const [name, symbol] = await Promise.all([
+        tokenContract.name(),
+        tokenContract.symbol()
+    ]);
+    return {name, symbol};
+}
+
+async function fetchGaugeInfo(gaugeAddresses, contract, provider, injectorAddress) {
+    const gaugeContracts = gaugeAddresses.map(address => new ethers.Contract(address, gaugeABI, provider));
+
+    const gaugeInfoPromises = gaugeAddresses.map(async (address, index) => {
+        const [accountInfo, lpToken] = await Promise.all([
+            contract.getAccountInfo(address),
+            gaugeContracts[index].lp_token()
+        ]);
+
+        return {
+            gaugeAddress: address,
+            accountInfo,
+            lpToken
+        };
+    });
+
+    const gaugeInfos = await Promise.all(gaugeInfoPromises);
+
+    const gauges = await Promise.all(gaugeInfos.map(async (info) => ({
+        gaugeAddress: info.gaugeAddress,
+        poolName: await fetchPoolName(info.lpToken, provider),
+        amountPerPeriod: formatTokenAmount(info.accountInfo.amountPerPeriod, injectorAddress),
+        maxPeriods: info.accountInfo.maxPeriods.toString(),
+        periodNumber: info.accountInfo.periodNumber.toString(),
+        lastInjectionTimeStamp: info.accountInfo.lastInjectionTimeStamp.toString()
+    })));
+
+    return gauges;
+}
+
+async function fetchPoolName(lpTokenAddress, provider) {
+    try {
+        const poolContract = new ethers.Contract(lpTokenAddress, poolsABI, provider);
+        return await poolContract.name();
+    } catch (error) {
+        console.error(`Error fetching pool name for address ${lpTokenAddress}:`, error);
+        return "Unknown Pool";
+    }
+}
+
+function formatTokenAmount(amount, tokenAddress) {
+    if (amount === null || amount === undefined) return "Loading...";
+
+    const formattedAmount = BigInt(amount.toString());
+    const decimals = tokenDecimals[tokenAddress.toLowerCase()] || 18;
+
+    return ethers.formatUnits(formattedAmount, decimals);
+}
--- app/rewards-injector/page.tsx
+++ app/rewards-injector/page.tsx
@@ -29,13 +29,7 @@ import GnosisLogo from '@/lib/shared/imgs/gnosis.svg'
 import BaseLogo from '@/lib/shared/imgs/base.svg'
 import MainnetLogo from '@/lib/shared/imgs/mainnet.svg'
 import zkevmLogo from '@/lib/shared/imgs/Polygon-zkEVM.png'
-import {ethers} from "ethers";
-import {InjectorABIV1} from "@/abi/InjectorV1";
-import {ERC20} from "@/abi/erc20";
-import {poolsABI} from "@/abi/pool";
-import {gaugeABI} from "@/abi/gauge";
 import {ChevronDownIcon} from "@chakra-ui/icons";
-import { Provider, Contract } from 'ethers-multicall';
 
 type AddressOption = {
     network: string;
@@ -57,18 +51,6 @@ type Recipient = {
     lastInjectionTimeStamp: string
 }
 
-const tokenDecimals: Record<string, number> = {
-    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": 6, // mainnet
-    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": 6, // polygon
-    "0xaf88d065e77c8cc2239327c5edb3a432268e5831": 6, // arbitrum
-    "0xddafbb505ad214d7b80b1f830fccc89b60fb7a83": 6, // gnosis
-    "0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035": 6, // zkevm
-    "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e": 6, // avalanche
-    "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913": 6, // base
-    "0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca": 6, // base USDbC
-    "0x0b2c639c533813f4aa9d7837caf62653d097ff85": 6, // OP USDC
-};
-
 const networks: Record<string, NetworkInfo> = {
     mainnet: {logo: MainnetLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=ethereum&dkey="},
     polygon: {logo: PolygonLogo.src, rpc: "https://lb.drpc.org/ogrpc?network=polygon&dkey="},
@@ -83,8 +65,6 @@ const networks: Record<string, NetworkInfo> = {
 function App() {
     const [addresses, setAddresses] = useState<AddressOption[]>([]);
     const [selectedAddress, setSelectedAddress] = useState<AddressOption | null>(null);
-    const [provider, setProvider] = useState<ethers.JsonRpcProvider>();
-    const [contract, setContract] = useState<ethers.Contract>();
     const [gauges, setGauges] = useState<Recipient[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const [isV2, setIsV2] = useState(false);
@@ -99,111 +79,33 @@ function App() {
 
     useEffect(() => {
         if (selectedAddress) {
-            getWatchList();
+            fetchInjectorData(selectedAddress.address,selectedAddress.network, selectedAddress.token)
         }
     }, [selectedAddress]);
 
     const handleAddressSelect = (address: AddressOption) => {
         setSelectedAddress(address);
-        const rpcUrl = `${networks[address.network].rpc}${process.env.NEXT_PUBLIC_DRPC_API_KEY}`;
-        const tempProvider = new ethers.JsonRpcProvider(rpcUrl);
-        setProvider(tempProvider);
-        setContract(new ethers.Contract(address.address, InjectorABIV1, tempProvider));
+        fetchInjectorData(address.address, address.network, address.token);
+
         setIsLoading(true);
     };
 
-    function formatTokenAmount(amount: any, tokenAddress: string) {
-        if (amount === null || amount === undefined) return "Loading...";
 
-        const formattedAmount = BigInt(amount.toString());
-        const decimals = tokenDecimals[tokenAddress.toLowerCase()] || 18;
 
-        return ethers.formatUnits(formattedAmount, decimals);
-    }
-
-    async function getWatchList() {
-        if (contract && provider) {
-            try {
-                const [watchList, injectorTokenAddress] = await Promise.all([
-                    contract.getWatchList(),
-                    contract.getInjectTokenAddress()
-                ]);
-
-                await fetchTokenInfo(injectorTokenAddress);
-
-                const newRecipients: Recipient[] = await fetchGaugeInfo(watchList);
-
-                setGauges(newRecipients);
-                setIsLoading(false);
-            } catch (error) {
-                console.error("Error:", error);
-                setIsLoading(false);
-            }
-        }
-    }
-
-    async function fetchGaugeInfo(gaugeAddresses: string[]) {
-        if (!provider || !contract) return [];
-
-        const gaugeContracts = gaugeAddresses.map(address => new ethers.Contract(address, gaugeABI, provider));
-
-        const gaugeInfoPromises = gaugeAddresses.map(async (address, index) => {
-            const [accountInfo, lpToken] = await Promise.all([
-                contract.getAccountInfo(address),
-                gaugeContracts[index].lp_token()
-            ]);
-
-            return {
-                gaugeAddress: address,
-                accountInfo,
-                lpToken
-            };
-        });
-
-        const gaugeInfos = await Promise.all(gaugeInfoPromises);
-
-        const newRecipients: Recipient[] = await Promise.all(gaugeInfos.map(async (info) => ({
-            gaugeAddress: info.gaugeAddress,
-            poolName: await fetchPoolName(info.lpToken),
-            amountPerPeriod: formatTokenAmount(info.accountInfo.amountPerPeriod, selectedAddress!.address),
-            maxPeriods: info.accountInfo.maxPeriods.toString(),
-            periodNumber: info.accountInfo.periodNumber.toString(),
-            lastInjectionTimeStamp: info.accountInfo.lastInjectionTimeStamp.toString()
-        })));
-
-        return newRecipients;
-    }
-
-    async function fetchPoolName(lpTokenAddress: string) {
-        if (!provider) return "Unknown Pool";
-
+    async function fetchInjectorData(address, network, token) {
+        setIsLoading(true);
         try {
-            const poolContract = new ethers.Contract(lpTokenAddress, poolsABI, provider);
-            const poolName = await poolContract.name();
-            return poolName;
+            const response = await fetch(`/api/injector?address=${address}&network=${network}&token=${token}`);
+            const data = await response.json();
+            setTokenName(data.tokenInfo.name);
+            setTokenSymbol(data.tokenInfo.symbol);
+            setGauges(data.gauges);
         } catch (error) {
-            console.error(`Error fetching pool name for address ${lpTokenAddress}:`, error);
-            return "Unknown Pool";
+            console.error("Error fetching injector data:", error);
         }
+        setIsLoading(false);
     }
 
-    async function fetchTokenInfo(tokenAddress: string) {
-        if (!provider) return;
-
-        try {
-            const tokenContract = new ethers.Contract(tokenAddress, ERC20, provider);
-            const [name, symbol] = await Promise.all([
-                tokenContract.name(),
-                tokenContract.symbol()
-            ]);
-            setTokenName(name);
-            setTokenSymbol(symbol);
-        } catch (error) {
-            console.error("Error fetching token info:", error);
-            setTokenName("Unknown");
-            setTokenSymbol("???");
-        }
-    }
 
     useEffect(() => {
         fetch("https://raw.githubusercontent.com/BalancerMaxis/bal_addresses/main/outputs/addressbook.json")
